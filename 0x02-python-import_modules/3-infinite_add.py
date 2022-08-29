0a44af434357dcda80b994fe352d759dca27fde9
Yamimike <88089912+Yamimike@users.noreply.github.com>
2022-08-29 08:12:20 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    num_args = len(argv)
-    total = 0
-    for i in range(1, num_args):
-        total += int(argv[i])
-    print("{:d}".format(total))
+
+    import sys
+
+    value = 0
+    for i in range(len(sys.argv) - 1):
+        value = value + int(sys.argv[i + 1])
+    print("{}".format(value))
